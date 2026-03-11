88cd5581bc05ac9e69a8780f224890456039756c
= <dasha13111997@gmail.com>
2026-03-11 17:06:04 +0100

    #029 - css improvements

--- resources/js/components/WidgetList.jsx
+++ resources/js/components/WidgetList.jsx
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 function WidgetList({ widgets, onSelect }) {
   return (
     <div>
-      <div>Widgets</div>
+      <div className = "title-widget">Widgets</div>
       <div className = "grid grid-cols-3 gap-4">
         {widgets.map((widget) => (
           <div
             id = {widget.id}
             key = {widget.id}
             onClick = {() => onSelect(widget)}
-            className = "p-4 border rounded-md shadow cursor-pointer hover:bg-gray-50"
+            className = "p-4 w-60 h-70 border rounded-md shadow cursor-pointer hover:bg-gray-50"
           >
             <h2 className = "font-semibold">{widget.name}</h2>
             <p className = "text-sm text-gray-400">{widget.description}</p>
