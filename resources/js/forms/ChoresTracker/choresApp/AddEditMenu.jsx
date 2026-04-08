82a9af40629c752cf8dafdf64b040ffd0d9046ef
= <dasha13111997@gmail.com>
2026-04-08 17:57:19 +0200

    #02-009 Created add-edit form for chores item

--- resources/js/forms/ChoresTracker/choresApp/AddEditMenu.jsx
+++ resources/js/forms/ChoresTracker/choresApp/AddEditMenu.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdAddCircleOutline } from "react-icons/md";
 import { MdEdit } from "react-icons/md";
 import { MdDeleteOutline } from "react-icons/md";
 import { MdShare } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
-
+import ChoresItem from "./ChoresItem";
 import './choresApp.css';
 
 export default function AddEditMenu() {
+    
+    const [showForm, setShowForm] = useState();
+    const [noteId, setNoteId] = useState();
+    const openAddEditForm = () => {
+        setShowForm(true);
+    };
     return (
-        <div className="add-edit-menu">
-            <div className="add-edit-menu-icon"><MdAddCircleOutline /></div>
-            <div className="add-edit-menu-icon"><MdEdit /></div>
-            <div className="add-edit-menu-icon"><MdDeleteOutline /></div>
-            <div className="add-edit-menu-icon"><MdShare /></div>
+        <div>
+            <div className="add-edit-menu">
+                <div className="add-edit-menu-icon" onClick={openAddEditForm}><MdAddCircleOutline /></div>
+                <div className="add-edit-menu-icon" onClick={openAddEditForm}><MdEdit /></div>
+                <div className="add-edit-menu-icon"><MdDeleteOutline /></div>
+                <div className="add-edit-menu-icon"><MdShare /></div>
+            </div>
+            {showForm && (<ChoresItem noteId={noteId} />)}
         </div>
     );
 }
--- resources/js/forms/ChoresTracker/choresApp/ChoresItem.jsx
+++ resources/js/forms/ChoresTracker/choresApp/ChoresItem.jsx
@@ -1,9 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoIosCloseCircle } from "react-icons/io";
 
-export default function ChoresItem() {
+import './choresApp.css';
+export default function ChoresItem(noteId) {
+
+    const [categorie, setCategorie] = useState('');
+    const [header, setHeader] = useState('');
+    const [body, setBody] = useState('');
+    const [date, setDate] = useState();
+    const [time, setTime] = useState();
+    const [dueDate, setDuedate] = useState('');
+    const [description, setDuescription] = useState('');
+    const [cost, setCost] = useState('');
+
+    const closeForm = () => {
+
+    };
     return (
         <div className="chores-item">
-
+            <div className="chores-item-header">
+                <span className="chores-item-header-title">Add item </span>
+                <div className="close-form" onClick={closeForm}>
+                    <IoIosCloseCircle />
+                </div>
+            </div>
+            <hr />
+            <div className="chores-item-form">
+                <input
+                    type="text"
+                    id="header"
+                    className="chores-item-form-header"
+                    placeholder="Type header..."
+                    onChange={(e) => setHeader(e.target.value)}
+                />
+                <textarea
+                    id="body"
+                    className="chores-item-form-body"
+                    placeholder="Insert text..."
+                    onChange={(e) => setBody(e.target.value)}
+                />
+                <div className="chores-item-form-datetime-block">
+                    <input
+                        type="date"
+                        className="chores-item-form-date"
+                        placeholder="DD.MM.YY"
+                        onChange={(e) => setDate(e.target.value)}
+                    />
+                    <input
+                        type="time"
+                        className="chores-item-form-time"
+                        id="time"
+                        name="time"
+                        onChange={(e) => setTime(e.target.value)}
+                    />
+                </div>
+                <div className="chores-item-categories"></div>
+            </div>
+            <div className="chores-item-footer">
+                <button>Save</button>
+            </div>
         </div>
     );
 }
