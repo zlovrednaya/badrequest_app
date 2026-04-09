6232ee1e44d3809e13791fcb0ca04168541a58cf
= <dasha13111997@gmail.com>
2026-04-09 20:49:38 +0200

    #02-010 JS, CSS - improved chores item

--- resources/js/forms/ChoresTracker/choresApp/ChoresItem.jsx
+++ resources/js/forms/ChoresTracker/choresApp/ChoresItem.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { IoIosCloseCircle } from "react-icons/io";
+import { SlStar } from "react-icons/sl";
+
 
 import './choresApp.css';
+import './choresItem.css';
 export default function ChoresItem(noteId) {
 
     const [categorie, setCategorie] = useState('');
@@ -9,17 +12,30 @@ export default function ChoresItem(noteId) {
     const [body, setBody] = useState('');
     const [date, setDate] = useState();
     const [time, setTime] = useState();
-    const [dueDate, setDuedate] = useState('');
-    const [description, setDuescription] = useState('');
+    const [category, setCategory] = useState();
+    const [color, setColor] = useState();
     const [cost, setCost] = useState('');
 
+    const categoryList = [
+        {'name': 'Home'},
+        {'name': 'Schedule'},
+        {'name': 'Notes'},
+    ];
+
+    const colorList = [
+        {'color': '#ff99c8', 'className': 'color-rose', 'name': 'Rose',},
+        {'color': '#fcf6bd', 'className': 'color-yellow', 'name': 'Yellow'},
+        {'color': '#d0f4de', 'className': 'color-green', 'name': 'Pastel green'},
+        {'color': '#a9def9', 'className': 'color-blue', 'name': 'Blue'},
+        {'color': '#e4c1f9', 'className': 'color-violet', 'name': 'Violet'},
+    ]
     const closeForm = () => {
 
     };
     return (
         <div className="chores-item">
             <div className="chores-item-header">
-                <span className="chores-item-header-title">Add item </span>
+                <span className="chores-item-header-title">Add chore </span>
                 <div className="close-form" onClick={closeForm}>
                     <IoIosCloseCircle />
                 </div>
@@ -54,7 +70,42 @@ export default function ChoresItem(noteId) {
                         onChange={(e) => setTime(e.target.value)}
                     />
                 </div>
-                <div className="chores-item-categories"></div>
+                <div className="chores-item-categories">
+                    <label for="category">Category: </label>
+                    <input 
+                        placeholder="Choose category" 
+                        list="category"
+                        onChange={(e) => setCategory(e.target.value)}
+                    />
+                    <datalist id="category">
+                        {categoryList.map((category) => (
+                            <option>{category.name}</option>
+                        ))}
+                    </datalist>
+                </div>
+                <div className="chores-item-color">
+                    <label for="color">Color: </label>
+                    <input 
+                        placeholder="Choose color" 
+                        list="color"
+                        onChange={(e) => setColor(e.target.value)}
+                    />
+                    <datalist id="color">
+                        {colorList.map((color) => (
+                            <option className={color.className}>{color.name}</option>
+                        ))}
+                    </datalist>
+                </div>
+                <div className="chores-item-cost">
+                    <input
+                        type="text"
+                        id="cost"
+                        className="chores-item-form-cost"
+                        placeholder="cost.."
+                        onChange={(e) => setCost(e.target.value)}
+                    />
+                    <label for="cost"><SlStar /> </label>
+                </div>
             </div>
             <div className="chores-item-footer">
                 <button>Save</button>
